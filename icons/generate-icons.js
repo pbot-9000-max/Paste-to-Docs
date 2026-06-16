f305fd7942d0161baf3d4d0851f05a149811cd55
prasanna <sudprakum@gmail.com>
2026-06-16 17:31:40 +0530

    chore: add app icons and icon generator

---
+++ icons/generate-icons.js
@@ -0,0 +1,23 @@
+/**
+ * generate-icons.js
+ * Converts icons/icon.svg into the PNG sizes required by Chrome Web Store.
+ *
+ * Usage:
+ *   npm install sharp
+ *   node generate-icons.js
+ */
+
+const sharp = require('sharp');
+const path  = require('path');
+
+const SIZES = [16, 32, 48, 128];
+const src   = path.join(__dirname, 'icons', 'icon.svg');
+
+(async () => {
+  for (const size of SIZES) {
+    const dest = path.join(__dirname, 'icons', `icon${size}.png`);
+    await sharp(src).resize(size, size).png().toFile(dest);
+    console.log(`✓ icons/icon${size}.png`);
+  }
+  console.log('\nAll icons generated.');
+})();

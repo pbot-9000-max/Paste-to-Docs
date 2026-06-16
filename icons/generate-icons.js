1aa97cefe2d9a46c0f2fca9c294a84a34f9cdec0
prasanna <sudprakum@gmail.com>
2026-06-16 17:32:31 +0530

    chore: install dependencies and fix icon generator path

--- icons/generate-icons.js
+++ icons/generate-icons.js
@@ -11,11 +11,11 @@ const sharp = require('sharp');
 const path  = require('path');
 
 const SIZES = [16, 32, 48, 128];
-const src   = path.join(__dirname, 'icons', 'icon.svg');
+const src   = path.join(__dirname, 'icon.svg');
 
 (async () => {
   for (const size of SIZES) {
-    const dest = path.join(__dirname, 'icons', `icon${size}.png`);
+    const dest = path.join(__dirname, `icon${size}.png`);
     await sharp(src).resize(size, size).png().toFile(dest);
     console.log(`✓ icons/icon${size}.png`);
   }
